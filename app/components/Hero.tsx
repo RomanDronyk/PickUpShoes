70d12a2b7429cbed59d35ba5b251038b7800e693
Іван Калинич <ivankalynych@MacBook-Air-Ivan.local>
2024-06-06 14:36:54 +0300

    add padding for Виділяйся

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -16,9 +16,9 @@ export function Hero({heroData}: {heroData: HomeHeroQuery}) {
             {heading.value}
           </h1>
           <div className="mt-7">
-            <span className="inline-flex bg-white rounded-[30px] px-5 font-semibold text-black  text-[26px]  md:text-3xl lg:text-4xl  xl:text-[42px] mr-3">
-              Виділяйся
-            </span>
+          <span className="inline-flex bg-white rounded-[30px] px-5 py-2.5 font-semibold text-black text-[26px] md:text-3xl lg:text-4xl xl:text-[42px] mr-3">
+  Виділяйся
+</span>
             <span className="text-[#D70000] font-semibold text-[26px]  md:text-3xl lg:text-4xl xl:text-[42px]">
               з комфортом
             </span>
